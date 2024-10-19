b7f74dbd91d5911d1e3adacf8b0c5d5d52f385aa
Verifieddanny <dannyclassic56@gmail.com>
2024-10-19 11:13:04 +0100

    [Fix: removed unused font]

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,16 +13,16 @@ const poppins = Poppins({
   weight: ["100" , "200" , "300" , "400" , "500" , "600" , "700" , "800" , "900"]
 })
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+// const geistSans = localFont({
+//   src: "./fonts/GeistVF.woff",
+//   variable: "--font-geist-sans",
+//   weight: "100 900",
+// });
+// const geistMono = localFont({
+//   src: "./fonts/GeistMonoVF.woff",
+//   variable: "--font-geist-mono",
+//   weight: "100 900",
+// });
 
 // export const metadata: Metadata = {
 //   title: "Create Next App",
