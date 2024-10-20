2050d7ff059363bf9287ab9a0086d388945ed1a0
Verifieddanny <dannyclassic56@gmail.com>
2024-10-20 21:51:59 +0100

    [Fix: Listing page]

--- app/listing/page.tsx
+++ app/listing/page.tsx
@@ -30,12 +30,7 @@ function page() {
       </div>
 
 
-      <div className="bg-[#f1f1f1]  text-[#010101] md:py-24 py-4 lg:px-96 md:px-12 px-8 w-screen h-fit relative left-1/2 right-1/2 -mx-[50vw]">
-<p className="md:text-5xl text-3xl font-thin italic text-center md:mb-12 mb-6">City break in the heart of Chester</p>
-      <p className="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda earum, facere eligendi quasi repudiandae sequi, aspernatur amet architecto quaerat, tempore consequatur? Saepe reprehenderit error eos adipisci quasi veritatis laborum non. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti libero consequuntur ullam voluptate doloremque necessitatibus repellendus, voluptas accusamus vel repudiandae et, nesciunt autem unde porro ducimus culpa dolore quidem error.</p>
-      <p className="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda earum, facere eligendi quasi repudiandae sequi, aspernatur amet architecto quaerat, tempore consequatur? Saepe reprehenderit error eos adipisci quasi veritatis laborum non.</p>
-      
-      </div>
+
 
       <div className="bg-[#010101]  text-[#f1f1f1] md:py-24 py-4 lg:px-12 px-8 w-screen h-fit relative left-1/2 right-1/2 -mx-[50vw]">
       <div className="flex flex-col flex-wrap justify-center items-center gap-6 md:flex-row md:gap-x-12">
@@ -49,7 +44,7 @@ function page() {
             <p className='font-bold'>Entire Home</p>
             <p className='w-[23rem]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi quod ex assumenda minus excepturi voluptates odio similique dolorum natus, accusamus quam quaerat fugit aperiam nesciunt quasi nobis atque eius facere?</p>
 
-      <div className='py-2 px-4 rounded-md cursor-pointer flex justify-center items-center border-2 border-[#101010] hover:bg-white transition-all duration-300 mt-[2rem] w-fit mx-auto'>Book.</div>
+      <div className='py-2 px-4 rounded-md cursor-pointer flex justify-center items-center border-2 border-[#101010] hover:bg-white transition-all duration-300 mt-[2rem] w-fit mx-auto'>Read more...</div>
 
         </div>
         <div className='w-fit h-fit rounded-md overflow-hidden  bg-[#f1f1f1] text-[#010101] p-2'>
@@ -61,7 +56,7 @@ function page() {
             <p className='font-bold'>Town House</p>
             <p className='w-[23rem]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi quod ex assumenda minus excepturi voluptates odio similique dolorum natus, accusamus quam quaerat fugit aperiam nesciunt quasi nobis atque eius facere?</p>
 
-      <div className='py-2 px-4 rounded-md cursor-pointer flex justify-center items-center border-2 border-[#101010] hover:bg-white transition-all duration-300 mt-[2rem] w-fit mx-auto'>Book.</div>
+      <div className='py-2 px-4 rounded-md cursor-pointer flex justify-center items-center border-2 border-[#101010] hover:bg-white transition-all duration-300 mt-[2rem] w-fit mx-auto'>Read more...</div>
         </div>
         <div className='w-fit h-fit rounded-md overflow-hidden  bg-[#f1f1f1] text-[#010101] p-2'>
         <div className='w-[24rem] h-[15rem] rounded-md overflow-hidden relative mb-[4rem]'>
@@ -72,7 +67,7 @@ function page() {
             <p className='font-bold'>Entire Home</p>
             <p className='w-[23rem]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi quod ex assumenda minus excepturi voluptates odio similique dolorum natus, accusamus quam quaerat fugit aperiam nesciunt quasi nobis atque eius facere?</p>
 
-      <div className='py-2 px-4 rounded-md cursor-pointer flex justify-center items-center border-2 border-[#101010] hover:bg-white transition-all duration-300 mt-[2rem] w-fit mx-auto'>Book.</div>
+      <div className='py-2 px-4 rounded-md cursor-pointer flex justify-center items-center border-2 border-[#101010] hover:bg-white transition-all duration-300 mt-[2rem] w-fit mx-auto'>Read more...</div>
         </div>
         <div className='w-fit h-fit rounded-md overflow-hidden  bg-[#f1f1f1] text-[#010101] p-2'>
         <div className='w-[24rem] h-[15rem] rounded-md overflow-hidden relative mb-[4rem]'>
@@ -83,7 +78,7 @@ function page() {
             <p className='font-bold'>Town House </p>
             <p className='w-[23rem]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi quod ex assumenda minus excepturi voluptates odio similique dolorum natus, accusamus quam quaerat fugit aperiam nesciunt quasi nobis atque eius facere?</p>
 
-      <div className='py-2 px-4 rounded-md cursor-pointer flex justify-center items-center border-2 border-[#101010] hover:bg-white transition-all duration-300 mt-[2rem] w-fit mx-auto'>Book.</div>
+      <div className='py-2 px-4 rounded-md cursor-pointer flex justify-center items-center border-2 border-[#101010] hover:bg-white transition-all duration-300 mt-[2rem] w-fit mx-auto'>Read more...</div>
         </div>
 </div>
       </div>
